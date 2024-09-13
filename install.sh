5557f92fcf48bc466ce1e6c28d2d12ead67952be
LiGoZoff <0718snak0718@gmail.com>
2024-09-14 02:55:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S swaybg mako alacritty nemo rofi flatpak pavucontrol dnscrypt-proxy mpv imv htop fastfetch qjackctl neofetch cmatrix waybar wine qbittorrent polkit-kde-agent network-manager-applet steam file-roller nwg-look grim slurp hyprlock btop  ttf-font-awesome noto-fonts-emoji ttf-joypixels  noto-fonts noto-fonts-cjk noto-fonts-emoji xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal
+sudo pacman -S swaybg mako alacritty nemo rofi flatpak pavucontrol dnscrypt-proxy mpv imv htop fastfetch qjackctl neofetch cmatrix waybar wine qbittorrent network-manager-applet steam file-roller nwg-look hyprlock btop  ttf-font-awesome noto-fonts-emoji ttf-joypixels  noto-fonts noto-fonts-cjk noto-fonts-emoji xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal
 yay -S hyprpicker clipse cava google-chrome-stable ytdesk-git telegram-desktop-bin vesktop-bin yandex-music proton-vpn-gtk-app bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons ttf-twemoji otf-openmoji ttf-twemoji-color ttf-symbola
 flatpak install flathub org.gnome.TextEditor
 flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub io.github.Soundux
-sudo pacman -R dolphin kitty wofi
+sudo pacman -R dolphin kitty wofi dunst
 
