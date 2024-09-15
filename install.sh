661a680ef15b040aea61e6aacbb47ff38908b52c
LiGoZoff <0718snak0718@gmail.com>
2024-09-15 22:36:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S swaybg mako alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar wine qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome
+sudo pacman -S swaybg mako nemo alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar wine qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5
 yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music proton-vpn-gtk-app bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle swaylock bluez bluez-utils bluez-deprecated-tools cliphist
 flatpak install flathub org.gnome.TextEditor
 flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub io.github.Soundux
-sudo pacman -R wofi dunst vim kitty
+sudo pacman -R wofi dunst vim kitty dolphhin
 
