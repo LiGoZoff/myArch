677e472f8f25292c2afe5047a544b269cd40b576
LiGoZoff <0718snak0718@gmail.com>
2024-09-20 00:13:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S swaybg mako nemo alacritty rofi flatpak wine-mono wine-gecko lib32-gnutls pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar wine qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 winetricks dnsutils
+sudo pacman -S swaybg mako nemo alacritty wine rofi flatpak wine-mono wine-gecko lib32-gnutls pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 winetricks dnsutils
 yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music proton-vpn-gtk-app bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle swaylock bluez bluez-utils bluez-deprecated-tools cliphist
 flatpak install flathub org.gnome.TextEditor
 flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
