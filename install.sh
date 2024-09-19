717807e52cd6dc39d16b5177dfdef4dfb7194f6e
LiGoZoff <0718snak0718@gmail.com>
2024-09-20 00:17:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S swaybg mako nemo alacritty wine rofi flatpak wine-mono wine-gecko lib32-gnutls pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 winetricks dnsutils
-yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music proton-vpn-gtk-app bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle swaylock bluez bluez-utils bluez-deprecated-tools cliphist
+yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music proton-vpn-gtk-app bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist
 flatpak install flathub org.gnome.TextEditor
 flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
 flatpak install flathub com.github.tchx84.Flatseal
