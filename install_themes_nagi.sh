48535f0f908b4582965f9c52238ccb6969b940ec
LiGoZoff <0718snak0718@gmail.com>
2024-10-06 00:10:58 +0300

    Add files via upload

---
+++ install_themes_nagi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo rm -rf ~/.config/hypr
+sudo mv ~/myArch/.config/hypr ~/.config/
+sudo mv ~/myArch/.config/alacritty ~/.config/
+sudo mv ~/myArch/.config/mako ~/.config/
+sudo mv ~/myArch/.config/rofi ~/.config/
+
+sudo mv ~/myArch/themes/nagi/Pictures ~/
+sudo mv ~/myArch/themes/nagi/fastfetch ~/.config/
+sudo mv ~/myArch/themes/nagi/waybar ~/.config/
+sudo mv ~/myArch/themes/nagi/hyprland.conf ~/.config/hypr/
+
+sudo rm -rf ~/.bashrc
+sudo mv ~/myArch/themes/nagi/.bashrc ~/
+
+sudo mkdir .icons
+sudo mkdir .themes
+sudo mv ~/myArch/themes/nagi/Catppuccin-Dark ~/.themes
+sudo mv ~/myArch/themes/nagi/Catppuccin-Dark-hdpi ~/.themes
+sudo mv ~/myArch/themes/nagi/Catppuccin-Dark-xhdpi ~/.themes
+
+sudo mv ~/myArch/themes/nagi/oreo_spark_lite_cursors ~/.icons
+sudo mv ~/myArch/themes/nagi/ePapirus-Dark ~/.icons
+
+sudo cp -i /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
+sudo mv themes/simple-sddm /usr/share/sddm/themes/
---
+++ install_themes_satoru.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo rm -rf ~/.config/hypr
+sudo mv ~/myArch/.config/hypr ~/.config/
+sudo mv ~/myArch/.config/alacritty ~/.config/
+sudo mv ~/myArch/.config/mako ~/.config/
+sudo mv ~/myArch/.config/rofi ~/.config/
+
+sudo mv ~/myArch/themes/satoru/Pictures ~/
+sudo mv ~/myArch/themes/satoru/fastfetch ~/.config/
+sudo mv ~/myArch/themes/satoru/waybar ~/.config/
+sudo mv ~/myArch/themes/satoru/hyprland.conf ~/.config/hypr/
+
+sudo rm -rf ~/.bashrc
+sudo mv ~/myArch/themes/satoru/.bashrc ~/
+
+sudo mkdir .icons
+sudo mkdir .themes
+sudo mv ~/myArch/themes/satoru/Catppuccin-Dark ~/.themes
+sudo mv ~/myArch/themes/satoru/Catppuccin-Dark-hdpi ~/.themes
+sudo mv ~/myArch/themes/satoru/Catppuccin-Dark-xhdpi ~/.themes
+
+sudo mv ~/myArch/themes/satoru/oreo_spark_lite_cursors ~/.icons
+sudo mv ~/myArch/themes/satoru/ePapirus-Dark ~/.icons
+
+sudo cp -i /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
+sudo mv themes/simple-sddm /usr/share/sddm/themes/
