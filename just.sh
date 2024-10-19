d1b8758206f2b1fe5517b8a62e36442ec876b3ea
LiGoZoff <0718snak0718@gmail.com>
2024-10-19 16:21:37 +0300

    Add files via upload

---
+++ just.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+nmcli device wifi connect Xiaomi_0298_5G password 59563129
+
+cd /opt/
+sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R ligoz:ligoz yay/
+cd yay/; makepkg -si
+
+sudo rm -rf /etc/pacman.conf
+sudo mv ~/myArch/conf/pacman.conf /etc/
+
+sudo rm -rf /etc/local.gen
+sudo mv ~/myArch/conf/locale.gen
+sudo nano /etc/locale.gen
+sudo localectl set-locale ru_RU.UTF-8
+sudo locale-gen
+
+git clone https://github.com/LiGoZoff/myArch.git
+sudo chmod +x ~/myArch/.config/hypr/vscode.sh
+sudo chmod +x ~/myArch/.config/rofi/launchers/type-6/launcher.sh
+sudo chmod +x ~/myArch/.config/rofi/powermenu/type-4/powermenu.sh
+sudo chmod +x ~/myArch/install_themes_nagi.sh
+sudo chmod +x ~/myArch/install_themes_satoru.sh
+sudo chmod +x ~/myArch/install.sh
+./myArch/install.sh
+sudo rm -rf ~/.config/hypr/hyprland.conf
+sudo cp ~/myArch/themes/nagi/hyprland.conf ~/.config/hypr/
+
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
+sudo rfkill unblock all
+sudo mkdir /etc/init.d
+sudo mv ~/myArch/conf/rfkillunblock /etc/init.d/
+sudo chmod +x /etc/init.d/rfkillunblock
+sudo nano mv ~/myArch/conf/rfkillunblock.service /etc/systemd/system/
+sudo systemctl start rfkillunblock
+sudo systemctl enable rfkillunblock
+gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
+
+sudo cp -i /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
+sudo mv ~/myArch/conf/sddm.conf /etc/
+
+sudo cd /opt; sudo git clone https://github.com/bol-van/zapret.git
+cd zapret; sudo bash install_bin.sh
+sudo bash install_prereq.sh
+
+sudo chmod +x ~/myArch/conf/dpi.sh
+sudo bash ~/myArch/conf/dpi.sh
+
+sudo bash blockcheck.sh
+sudo bash install_easy.sh
