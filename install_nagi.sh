035d5d185c5ab00d8cb14b9617d9d0423dd1b5bd
LiGoZoff <0718snak0718@gmail.com>
2024-10-29 19:56:11 +0300

    Update install_nagi.sh

--- install_nagi.sh
+++ install_nagi.sh
@@ -51,17 +51,17 @@ bash ~/myArch/conf/dpi.sh
 bash ~/myArch/dpi/dpi.sh
 
 
-tar -xvzf ~/myArch/themes/nagi/Catppuccin-Dark-B-LB.tar.gz
+tar -xJf ~/myArch/themes/nagi/Kripton-v40.tar.xz
 tar -xvzf ~/myArch/themes/nagi/cursor.tar.gz 
-tar -xJf ~/myArch/themes/nagi/papirus-icon-theme-carmine-folders.tar.xz 
+tar -xJf ~/myArch/themes/nagi/papirus-icon-theme-white-folders.tar.xz 
 tar -xvzf ~/myArch/themes/nagi/simple-sddm.tar.gz 
 sudo rm -rf ~/Catppucin-Light
 
 sudo rm -rf ~/.config/hypr
 sudo mv ~/myArch/.config/hypr ~/.config/
 sudo mv ~/myArch/.config/alacritty ~/.config/
-sudo mv ~/myArch/.config/mako ~/.config/
-sudo mv ~/myArch/.config/rofi ~/.config/
+sudo mv ~/myArch/themes/nagi/mako ~/.config/
+sudo mv ~/myArch/themes/nagi/rofi ~/.config/
 
 sudo mv ~/myArch/themes/nagi/Pictures ~/
 sudo mv ~/myArch/themes/nagi/fastfetch ~/.config/
@@ -74,9 +74,7 @@ sudo mv ~/myArch/.bashrc /root
 
 mkdir .icons
 mkdir .themes
-sudo mv ~/Catppuccin-Dark ~/.themes
-sudo mv ~/Catppuccin-Dark-hdpi ~/.themes
-sudo mv ~/Catppuccin-Dark-xhdpi ~/.themes
+sudo mv ~/Kripton-v40 ~/.themes
 
 sudo mv ~/oreo_spark_lite_cursors ~/.icons
 sudo mv ~/ePapirus-Dark ~/.icons
