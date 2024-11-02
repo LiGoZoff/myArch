c549821662d02c081aa67f57f0b40800d7847fdc
LiGoZoff <0718snak0718@gmail.com>
2024-11-03 01:10:12 +0300

    Update dpi.sh

--- dpi/dpi.sh
+++ dpi/dpi.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo rm -rf /opt/zapret/config
-sudo mv ~/myArch/dpi/config /opt/zapret/
-sudo rm -rf /opt/zapret/ipset/zapret-hosts-user.txt
-sudo mv ~/myArch/dpi/zapret-hosts-user.txt /opt/zapret/ipset/
-sudo rm -rf /opt/zapret/init.d/sysv/custom
-sudo mv ~/myArch/dpi/custom /opt/zapret/init.d/sysv/
+sudo mv /opt/zapret/init.d/sysv/custom.d.examples/50-discord /opt/zapret/init.d/sysv/custom.d/
+sudo rm -rf /opt/zapret/config.default
+sudo mv /home/ligoz/myArch/dpi/config.default /opt/zapret/
+
 cd /opt/zapret
 sudo bash install_easy.sh
