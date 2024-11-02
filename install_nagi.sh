dd839966167ecfe3918361b7d9c9b871ffc11150
LiGoZoff <0718snak0718@gmail.com>
2024-11-03 01:14:53 +0300

    Update install_nagi.sh

--- install_nagi.sh
+++ install_nagi.sh
@@ -20,12 +20,7 @@ sudo mv ~/myArch/conf/locale.gen /etc/
 sudo localectl set-locale ru_RU.UTF-8
 sudo locale-gen
 
-cd ~
-sudo pacman -S swaybg mako nemo alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils webkit2gtk
-yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist visual-studio-code-bin
-flatpak install flathub net.lutris.Lutris
-sudo pacman -Rns wofi dunst vim dolphin
-
+sudo bash ~/myArch/scripts/install.shn
 
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
