8db85d3a2c8b44baf9c44829a48a5922965a9542
LiGoZoff <0718snak0718@gmail.com>
2024-11-03 01:20:41 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd ~
 sudo pacman -S swaybg mako nemo alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils webkit2gtk
 yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist visual-studio-code-bin
