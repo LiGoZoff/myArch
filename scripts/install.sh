e6bb598102b9c7ea4f393e5922e46bae7f3635d9
LiGoZoff <0718snak0718@gmail.com>
2024-11-03 01:12:40 +0300

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+cd ~
+sudo pacman -S swaybg mako nemo alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils webkit2gtk
+yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist visual-studio-code-bin
+flatpak install flathub net.lutris.Lutris
+sudo pacman -Rns wofi dunst vim dolphin
