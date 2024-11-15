194956d752e18421755f8a7733e3fb8e685633cc
LiGoZoff <0718snak0718@gmail.com>
2024-11-16 02:26:57 +0300

    Delete dpi directory

--- dpi/dpi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo mv /opt/zapret/init.d/sysv/custom.d.examples/50-discord /opt/zapret/init.d/sysv/custom.d/
-sudo rm -rf /opt/zapret/config.default
-sudo mv /home/ligoz/myArch/dpi/config.default /opt/zapret/
-
-cd /opt/zapret
-sudo bash install_easy.sh
