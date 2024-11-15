8b7a949837ed3e5d3ace3d02aaf658f518b6204b
LiGoZoff <0718snak0718@gmail.com>
2024-11-16 02:31:19 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,6 +13,12 @@ sudo mv ~/myArch/conf/locale.gen /etc/
 sudo localectl set-locale ru_RU.UTF-8
 sudo locale-gen
 
+cd ~
+sudo pacman -S swaybg mako nemo alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils webkit2gtk
+yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist visual-studio-code-bin
+flatpak install flathub net.lutris.Lutris
+sudo pacman -Rns wofi dunst vim dolphin
+
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
 sudo rfkill unblock all
@@ -26,9 +32,3 @@ gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
 
 sudo cp -i /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
 sudo mv ~/myArch/conf/sddm.conf /etc/
-
-cd ~
-sudo pacman -S swaybg mako nemo alacritty rofi flatpak pavucontrol dnscrypt-proxy imv htop fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils webkit2gtk
-yay -S hyprpicker clipse cava google-chrome youtube-music-bin telegram-desktop-bin vesktop-bin yandex-music bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist visual-studio-code-bin
-flatpak install flathub net.lutris.Lutris
-sudo pacman -Rns wofi dunst vim dolphin
