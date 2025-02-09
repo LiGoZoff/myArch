b5cacbb83b0aebd24cfca312947a9a419d1d5157
LiGoZoff <0718snak0718@gmail.com>
2025-02-09 07:52:06 +0300

    Update .swengine_after_run.sh

--- conf/.swengine_after_run.sh
+++ conf/.swengine_after_run.sh
@@ -1,5 +1,4 @@
 cp "$1" $HOME/Pictures/Wallpapers/.wallpaper_current
 cp "$1" /usr/share/sddm/themes/simple-sddm/Backgrounds/wallpaper_current.gif
 
-cd Pictures/Wallpapers
-wal -i .wallpaper_current
+./.config/hypr/wallpapers.sh
