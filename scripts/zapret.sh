9c7b5871f05d342240d422b7590306c840ca26ee
LiGoZoff <0718snak0718@gmail.com>
2025-03-15 14:45:38 +0300

    Update zapret.sh

--- scripts/zapret.sh
+++ scripts/zapret.sh
@@ -5,6 +5,3 @@ sudo bash ~/myArch/scripts/dpi.sh
 tar -xvzf ~/myArch/conf/zapret.tar.gz
 sudo mv ~/zapret /opt/
 cd /opt/zapret
-sudo bash install_bin.sh
-sudo bash install_prereq.sh
-sudo bash install_easy.sh
