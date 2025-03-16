6cfe7943a26d01af79f7c993a99c6b9206e9e6f2
LiGoZoff <0718snak0718@gmail.com>
2025-03-16 15:44:34 +0000

    Update zapret.sh

--- scripts/zapret.sh
+++ scripts/zapret.sh
@@ -4,3 +4,5 @@ sudo chmod +x ~/myArch/scripts/dpi.sh
 sudo bash ~/myArch/scripts/dpi.sh
 git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git
 sudo mv myArch/conf/conf.env zapret-discord-youtube-linux/
+cd zapret-discord-youtube-linux
+sudo bash service.sh
