9bd18ae1bbe43550aaea301d3fed74747d2b8411
LiGoZoff <0718snak0718@gmail.com>
2025-03-16 15:39:03 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,7 @@ sudo localectl set-locale ru_RU.UTF-8
 sudo locale-gen
 
 cd ~
-sudo pacman -S mako dotnet-sdk nemo rofi ibus pavucontrol dnscrypt-proxy python-pywal imv htop proton-vpn-gtk-app fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils telegram-desktop webkit2gtk
+sudo pacman -S mako dotnet-sdk nemo rofi nftables ibus pavucontrol dnscrypt-proxy python-pywal imv htop proton-vpn-gtk-app fastfetch qjackctl neofetch cmatrix waybar qbittorrent network-manager-applet steam file-roller nwg-look btop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-hannom xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal ttf-font-awesome plasma-framework5 lib32-sdl2 dnsutils telegram-desktop webkit2gtk
 yay -S hyprpicker swww-git swengine clipse cava google-chrome youtube-music-bin vesktop-bin yandex-music bluetuith-bin onlyoffice-bin osu-lazer-bin ttf-font-icons ttf-ionicons hyprlock hypridle bluez bluez-utils bluez-deprecated-tools cliphist visual-studio-code-bin spotify portproton
 sudo pacman -Rns wofi dunst vim dolphin
 
