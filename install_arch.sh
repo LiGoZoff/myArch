42b243c3b5f5a0961ed51c379a32c16269ff76a7
LiGoZoff <0718snak0718@gmail.com>
2025-03-21 07:10:56 +0000

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -6,43 +6,6 @@ git clone https://github.com/LiGoZoff/myArch.git
 sudo chmod +x ~/myArch/scripts/install.sh
 ./myArch/scripts/install.sh
 
-
-tar -xJf ~/myArch/themes/Kripton-v40.tar.xz
-tar -xvzf ~/myArch/themes/cursor.tar.gz 
-tar -xJf ~/myArch/themes/papirus-icon-theme-white-folders.tar.xz 
-tar -xvzf ~/myArch/themes/simple-sddm.tar.gz 
-sudo rm -rf ~/Papirus-Light
-
-sudo rm -rf ~/.config/hypr
-sudo mv ~/myArch/.config/hypr ~/.config/
-sudo mv ~/myArch/.config/wal ~/.config/
-sudo mv ~/myArch/.config/kitty ~/.config/
-sudo mv ~/myArch/.config/fastfetch ~/.config/
-sudo mv ~/myArch/.config/mako ~/.config/
-sudo mv ~/myArch/.config/rofi ~/.config/
-sudo mv ~/myArch/.config/waybar ~/.config/
-sudo mv ~/myArch/.config/clipse ~/.config/
-sudo mv ~/myArch/conf/.swengine_after_run.sh ~/
-mkdir Pictures; mkdir Pictures/Wallpapers; mkdir Pictures/Screenshots
-
-sudo rm -rf ~/.bashrc
-sudo mv ~/myArch/themes/.bashrc ~/
-sudo mv ~/myArch/.bashrc /root
-
-mkdir .icons
-mkdir .themes
-sudo mv ~/Kripton-v40 ~/.themes
-
-sudo mv ~/oreo_spark_lite_cursors ~/.icons
-sudo mv ~/ePapirus-Dark ~/.icons
-sudo mv ~/ePapirus ~/.icons
-sudo mv ~/Papirus-Dark ~/.icons
-sudo mv ~/Papirus ~/.icons
-
-sudo mv ~/simple-sddm /usr/share/sddm/themes/
-
-./myArch/scripts/zapret.sh
-
 sudo rm -rf ~/myArch
 sudo rm -rf ~/install_arch.sh
 
