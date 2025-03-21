2c1815e0542ab7cf29405f86927f5ddd789f7f31
LiGoZoff <0718snak0718@gmail.com>
2025-03-21 07:11:02 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo chmod +x ~/myArch/.config/hypr/vscode.sh
+sudo chmod +x ~/myArch/.config/rofi/launcher.sh
+sudo chmod +x ~/myArch/scripts/zapret.sh
+sudo chmod +x ~/myArch/.config/hypr/wallpaper.sh
+
 cd /opt/
 sudo git clone https://aur.archlinux.org/yay.git
 sudo chown -R ligoz:ligoz yay/
@@ -32,3 +37,39 @@ gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
 
 sudo cp -i /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
 sudo mv ~/myArch/conf/sddm.conf /etc/
+
+tar -xJf ~/myArch/themes/Kripton-v40.tar.xz
+tar -xvzf ~/myArch/themes/cursor.tar.gz 
+tar -xJf ~/myArch/themes/papirus-icon-theme-white-folders.tar.xz 
+tar -xvzf ~/myArch/themes/simple-sddm.tar.gz 
+sudo rm -rf ~/Papirus-Light
+
+sudo rm -rf ~/.config/hypr
+sudo mv ~/myArch/.config/hypr ~/.config/
+sudo mv ~/myArch/.config/wal ~/.config/
+sudo mv ~/myArch/.config/kitty ~/.config/
+sudo mv ~/myArch/.config/fastfetch ~/.config/
+sudo mv ~/myArch/.config/mako ~/.config/
+sudo mv ~/myArch/.config/rofi ~/.config/
+sudo mv ~/myArch/.config/waybar ~/.config/
+sudo mv ~/myArch/.config/clipse ~/.config/
+sudo mv ~/myArch/conf/.swengine_after_run.sh ~/
+mkdir Pictures; mkdir Pictures/Wallpapers; mkdir Pictures/Screenshots
+
+sudo rm -rf ~/.bashrc
+sudo mv ~/myArch/themes/.bashrc ~/
+sudo mv ~/myArch/.bashrc /root
+
+mkdir .icons
+mkdir .themes
+sudo mv ~/Kripton-v40 ~/.themes
+
+sudo mv ~/oreo_spark_lite_cursors ~/.icons
+sudo mv ~/ePapirus-Dark ~/.icons
+sudo mv ~/ePapirus ~/.icons
+sudo mv ~/Papirus-Dark ~/.icons
+sudo mv ~/Papirus ~/.icons
+
+sudo mv ~/simple-sddm /usr/share/sddm/themes/
+
+./myArch/scripts/zapret.sh
