881de6e4ae662a6747328357ccf3a2d20b421103
LiGoZoff <0718snak0718@gmail.com>
2025-04-11 18:55:51 +0000

    Update .swengine_after_run.sh

--- conf/.swengine_after_run.sh
+++ conf/.swengine_after_run.sh
@@ -1,2 +1,12 @@
 cp "$1" $HOME/Pictures/Wallpapers/.wallpaper_current
 cp "$1" /usr/share/sddm/themes/simple-sddm/Backgrounds/wallpaper_current.gif
+
+pkill -SIGUSR1 waybar
+rm -rf .cache/wal
+cd Pictures/Wallpapers
+wal -i .wallpaper_current
+pkill -SIGUSR2 waybar
+pkill -SIGUSR1 waybar
+ln -s $HOME/.cache/wal/colors-discord.css $HOME/.config/vesktop/themes/pywal-vencord.theme.css
+bash $HOME/.config/hypr/chrome/generate-theme.sh
+hyprctl reload
