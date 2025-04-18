c035fe46ac24cc95a31e93f4f60205b11da16f1d
LiGoZoff <0718snak0718@gmail.com>
2025-04-19 00:16:27 +0300

    Delete scripts/zapret.sh

--- scripts/zapret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo chmod +x ~/myArch/scripts/dpi.sh
-bash ~/myArch/scripts/dpi.sh
-cd ~
-git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git
-sudo mv myArch/conf/conf.env zapret-discord-youtube-linux/
