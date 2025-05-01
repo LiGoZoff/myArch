3c33c1286ef95088e4ec5fd537ea21f1cc72c5db
LiGoZoff <0718snak0718@gmail.com>
2025-05-02 02:01:30 +0300

    Update .swengine_after_run.sh

--- conf/.swengine_after_run.sh
+++ conf/.swengine_after_run.sh
@@ -2,11 +2,9 @@ cp "$1" $HOME/Pictures/Wallpapers/.wallpaper_current
 cp "$1" /usr/share/sddm/themes/simple-sddm/Backgrounds/wallpaper_current.gif
 
 pkill -SIGUSR1 waybar
-rm -rf .cache
+rm -rf $HOME/.cache/wal
 wal -i $HOME/Pictures/Wallpapers/.wallpaper_current
 pkill -SIGUSR2 waybar
 hyprctl reload
 pkill -SIGUSR1 waybar
-pywalfox
-ln -s $HOME/.cache/wal/colors-discord.css $HOME/.config/vesktop/themes/pywal-vencord.theme.css
-bash ~/.config/hypr/Themes/pywal-obsidian/pywal-obsidian.sh "~/Документы/Obsidian"
+bash ~/.config/hypr/Themes/pywal-obsidian/pywal-obsidian.sh "$HOME/Документы/Obsidian"
