377ddcff4c736eb0cbe1c95e4b2d407d6b891af1
LiGoZoff <0718snak0718@gmail.com>
2025-05-01 18:08:53 +0300

    Add files via upload

--- conf/.swengine_after_run.sh
+++ conf/.swengine_after_run.sh
@@ -2,12 +2,10 @@ cp "$1" $HOME/Pictures/Wallpapers/.wallpaper_current
 cp "$1" /usr/share/sddm/themes/simple-sddm/Backgrounds/wallpaper_current.gif
 
 pkill -SIGUSR1 waybar
-rm -rf .cache/wal
-cd Pictures/Wallpapers
-wal -i .wallpaper_current
+rm -rf .cache
+wal -i $HOME/Pictures/Wallpapers/.wallpaper_current
 pkill -SIGUSR2 waybar
 hyprctl reload
 pkill -SIGUSR1 waybar
 ln -s $HOME/.cache/wal/colors-discord.css $HOME/.config/vesktop/themes/pywal-vencord.theme.css
-bash $HOME/.config/hypr/Themes/Chrome/generate-theme.sh
 bash ~/.config/hypr/Themes/pywal-obsidian/pywal-obsidian.sh "~/Документы/Obsidian"
